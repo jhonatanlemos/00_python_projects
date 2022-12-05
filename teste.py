e417d761e3a318674a6a3f09521f3f3eb074ea08
jhonatanlemos <jhonatanpontes10@hotmail.com>
2022-12-04 23:18:59 -0300

    arquivo teste

--- teste.py
+++ teste.py
@@ -1,4 +1,3 @@
-print ("Hello World")
-
-print ("Olá Mundo")
-
+import pandas as pd
+arquivo_teste = pd.read_excel(r'C:\Users\jhonatan.lemos.gx2.GCSP-NTB071\OneDrive - DIAGNOSTICOS DA AMERICA S.A\Área de Trabalho\00.Diversos\01. Bases Boticario\Evolução Pisicologia 01.07.2019-31.07.2022.xlsx')
+arquivo_teste.head
